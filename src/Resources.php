615f1f12bb145e78f40e66441ed4dfd7b73f45da
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2024-10-16 15:06:11 +0200

    Introduce QA

--- src/Resources.php
+++ src/Resources.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace Mammatus\Kubernetes\Attributes;
 
+use Attribute;
 use Mammatus\Kubernetes\Contracts\AddOn;
 
-#[\Attribute(\Attribute::TARGET_CLASS)]
+use function ceil;
+
+#[Attribute(Attribute::TARGET_CLASS)]
 final readonly class Resources implements AddOn, AddOn\Container
 {
+    /**
+     * @param non-negative-int|float $cpu    Non-negative value in CPU cores
+     * @param non-negative-int|float $memory Non-negative value in GigaBytes
+     */
     public function __construct(
         public int|float $cpu,
         public int|float $memory,
-    )
-    {
+    ) {
     }
 
     public function type(): string
@@ -26,14 +32,15 @@ final readonly class Resources implements AddOn, AddOn\Container
         return 'mammatus.container.resources';
     }
 
+    /** @return array{type: string, helper: string, arguments: array{cpu: string, memory: string}} */
     public function jsonSerialize(): array
     {
         return [
-            'type' => 'container',
-            'helper' => 'mammatus.container.resources',
+            'type' => $this->type(),
+            'helper' => $this->helper(),
             'arguments' => [
-                'cpu' => ($this->cpu * 1000) . 'm',
-                'memory' => ($this->memory * 1024) . 'Mi',
+                'cpu' => ceil($this->cpu * 1000) . 'm',
+                'memory' => ceil($this->memory * 1024) . 'Mi',
             ],
         ];
     }
